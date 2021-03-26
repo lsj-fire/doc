ab202c874664b4afaf91321f83bbc1f116c41c0d
lsj-fire <2577046376@qq.com>
2021-03-26 09:40:15 +0800

    repair test.c

---
+++ test.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "string.h"
+
+int main(){
+	char a;
+	int b;
+	double c;
+	long long d;
+	printf("char is:%d\nint is:%d\n",(int)sizeof(a),(int)sizeof(b));
+	return 0;
+}
